05722ead0533c4ae4cdff6a3155ef501af22bb79
wcornejo <William.Cornejo@nrel.gov>
2025-04-29 00:01:30 -0400

    sidebar

--- js/map_init.js
+++ js/map_init.js
@@ -9,21 +9,36 @@ let benzeneLayer;
 let fullData;      // for storing full GeoJSON for filtering
 
 const map1 = L.map('map1',{fullscreenControl: true}).setView([40.7128, -74.0060], 12);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map1);
+var alidade_smooth_dark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(map1);
 
-const map2 = L.map('map2',{fullscreenControl: true}).setView([40.7128, -74.0060], 12);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map2);
+//const map2 = L.map('map2',{fullscreenControl: true}).setView([40.7128, -74.0060], 12);
+//L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//  attribution: '© OpenStreetMap contributors'
+//}).addTo(map2);
 
 // Popup per feature
 function onEachFeature(feature, layer) {
   layer.bindPopup(
     "PM 2.5 Levels: " + (feature.properties.particulate_matter_25 * 100).toFixed(1) + "%" +
     "<br>" +
-    "Benzene Levels: " + (feature.properties.benzene_concentration * 100).toFixed(1) + "%"
+    "Benzene Levels: " + (feature.properties.benzene_concentration * 100).toFixed(1) + "%"+
+    "<br>" + "------------------------------" + "<br>" +
+    "Traffic Truck Highway Levels: " + (feature.properties.traffic_truck_highways * 100).toFixed(1) + "%" +
+    "<br>" +
+    "Traffic Number Vehicles Levels: " + (feature.properties.traffic_number_vehicles * 100).toFixed(1) + "%" +
+    "<br>" + "------------------------------" + "<br>" +
+    "Asthma ED Rate Levels: " + (feature.properties.asthma_ed_rate* 100).toFixed(1) + "%"+
+    "<br>" +
+    "COCPD ED Rate Levels: " + (feature.properties.copd_ed_rate * 100).toFixed(1) + "%"
+
   );
   //hover color change
   layer.on("mouseover", (e) => {
@@ -37,7 +52,32 @@ function onEachFeature(feature, layer) {
   layer.on("mouseout", (e) => {
     benzeneLayer.resetStyle(e.target);
     pmLayer.setStyle(getPmStyle);
+    trafficTruckHighwaysLayer.setStyle(getthwayStyle);
   });
+}
+function getthwayStyle(feature) {
+    const tt = feature.properties.traffic_truck_highways;
+    return {
+    fillColor: tt > .75 ? '#CB1212' :
+                tt > .5 ? '#656C2D' :
+                tt > .25 ? '#76C244' :
+                '#FFEDA0',
+    weight: 0.25,
+    color: "#333",
+    fillOpacity: 0.9
+    };
+}
+function getttnv(feature) {
+    const pm = feature.properties.particulate_matter_25
+    return {
+    fillColor: pm > .75 ? '#CB1212' :
+                pm > .5 ? '#656C2D' :
+                pm > .25 ? '#76C244' :
+                '#FFEDA0',
+    weight: 0.25,
+    color: "#333",
+    fillOpacity: 0.9
+    };
 }
 function getBenzStyle(feature) {
     const benz = feature.properties.benzene_concentration;
@@ -48,7 +88,20 @@ function getBenzStyle(feature) {
                 "#e0ccff",
     weight: 0.25,
     color: "#333",
-    fillOpacity: 0.6
+    fillOpacity: 0.9
+    };
+}
+
+function getPmStyle(feature) {
+    const pm = feature.properties.particulate_matter_25
+    return {
+    fillColor: pm > .75 ? '#CB1212' :
+                pm > .5 ? '#656C2D' :
+                pm > .25 ? '#76C244' :
+                '#FFEDA0',
+    weight: 0.25,
+    color: "#333",
+    fillOpacity: 0.9
     };
 }
 function getPmStyle(feature) {
@@ -60,7 +113,7 @@ function getPmStyle(feature) {
                 '#FFEDA0',
     weight: 0.25,
     color: "#333",
-    fillOpacity: 0.6
+    fillOpacity: 0.9
     };
 }
 // Filtering logic
@@ -104,29 +157,42 @@ benzSlider.addEventListener("input", () => {
   benzValue.textContent = benzSlider.value;
   updateMapFilters();
 });
+
 // Load GeoJSON and store it for filtering
 $.getJSON('data/fdc_2023.geojson', function (data) {
-  fullData = data;
-
-  pmLayer = L.geoJSON(data, {
-    onEachFeature: onEachFeature,
-    style: getPmStyle,
-  }).bindTooltip((l) => {
-return l.feature.properties.city_town + " County";
-}).addTo(map1); 
+    fullData = data;
+    //pm and 2.5 layers
+    pmLayer = L.geoJSON(data, {
+        onEachFeature: onEachFeature,
+        style: getPmStyle,
+    }).bindTooltip((l) => {
+        return l.feature.properties.city_town + " County";
+    }).addTo(map1); 
     benzeneLayer = L.geoJSON(data, {
         style: getBenzStyle,
         onEachFeature: onEachFeature
     }).bindTooltip((l) => {
-return l.feature.properties.city_town + " County";
-}).addTo(map1);
-  map1.fitBounds(pmLayer.getBounds());
-              // Add layer toggle control
-const overlays = {
-"PM 2.5 Layer": pmLayer,
-"Benzene Layer": benzeneLayer
-};
-L.control.layers(overlays, null).addTo(map1);
+        return l.feature.properties.city_town + " County";
+    }).addTo(map1);
+    //traffic_truck_highways/traffic_number_vehicles
+    trafficTruckHighwaysLayer = L.geoJSON(data, {
+        style: getthwayStyle,
+        onEachFeature: onEachFeature
+    }).bindTooltip((l) => {
+        return l.feature.properties.city_town + " County";
+    }).addTo(map1);
+    map1.fitBounds(pmLayer.getBounds());
+
+    const overlays = {
+        "PM 2.5 Layer": pmLayer,
+        "Benzene Layer": benzeneLayer,
+        "Traffic Truck Highway Layer": trafficTruckHighwaysLayer
+    };
+    var baseLayers = {
+        "Alidade Smooth Dark": alidade_smooth_dark,
+        "OpenStreetMap": osm
+      };
+    L.control.layers(baseLayers, overlays).addTo(map1);
 });
 
 
